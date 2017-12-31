f4e6fd8641d3293de5e9a8b5af8da776551af0d6
home <home@home-PC>
2017-12-31 12:47:17 +0530

    changed format

--- src/utils/ReadWrite.java
+++ src/utils/ReadWrite.java
@@ -20,8 +20,8 @@ public class ReadWrite {
 			FileInputStream fis = new FileInputStream(src);
 			XSSFWorkbook wb = new XSSFWorkbook(fis);
 			XSSFSheet sh1 = wb.getSheetAt(0);
-			// System.out.println(sh1.getRow(0).getCell(0).getStringCellValue());
-			// System.out.println(sh1.getRow(0).getCell(1).getStringCellValue());
+			 // System.out.println(sh1.getRow(0).getCell(0).getStringCellValue());
+			 // System.out.println(sh1.getRow(0).getCell(1).getStringCellValue());
 			// System.out.println(sh1.getRow(1).getCell(0).getStringCellValue());
 			// System.out.println(sh1.getRow(1).getCell(1).getStringCellValue());
 			// System.out.println(sh1.getRow(2).getCell(0).getStringCellValue());
